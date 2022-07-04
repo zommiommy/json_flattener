3f3e81db71158874fafefa799c380cc7b4b4bdd2
Tommaso Fontana <tommaso.fontana@gmail.com>
2022-07-04 18:10:56 +0200

    removed old code

--- fuzz_raw_strings.py
+++ fuzz_raw_strings.py
@@ -6,32 +6,6 @@ with atheris.instrument_imports():
   import json
   import json_flattener
 
-def consume_json(fdp):
-    type_num = fdp.ConsumeIntInRange(0, 4)
-    if type_num == 0:
-        return None
-    elif type_num == 1:
-        return fdp.ConsumeBool()
-    elif type_num == 2:
-        return fdp.ConsumeInt(fdp.ConsumeIntInRange(0, 8))
-    elif type_num == 3:
-        return fdp.ConsumeFloat()
-    elif type_num == 4:
-        return fdp.ConsumeString(fdp.ConsumeIntInRange(0, 1024))
-    elif type_num == 5:
-        return {
-            fdp.ConsumeString(fd.ConsumeIntInRange(0, 1024)):consume_json(fdp)
-            for _ in fdp.ConsumeIntInRange(0, 1024)
-        }
-    elif type_num == 6:
-        return [
-            consume_json(fdp)
-            for _ in fdp.ConsumeIntInRange(0, 1024)
-        ]
-    else:
-        raise ValueError("Wrong generation of json with type_num {}".format(type_num))
-
-
 def harness(data):
     fdp = atheris.FuzzedDataProvider(data)
     
